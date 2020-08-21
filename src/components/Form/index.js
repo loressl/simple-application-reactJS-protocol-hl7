b9b3045eb4c02239053762e82825563d08bdac94
loressl <loryssl@hotmail.com>
2020-08-21 01:46:50 -0300

    Testes com os layouts

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -12,6 +12,7 @@ import {Grid,
 import {ArrowBackIos,ArrowForwardIos} from '@material-ui/icons';
 
 import ImgSPO2 from '../../assets/image/spo2.jpg';
+import ImgFC from '../../assets/image/fc.jpg';
 
 export default function Form(){
     const initialValues={
@@ -19,7 +20,7 @@ export default function Form(){
         lastName:'',
         diagnostic:'',
         spo2:'0',
-        fc:''
+        fc:'0'
     }
     const [values, setValues]= useState(initialValues);
 
@@ -29,7 +30,91 @@ export default function Form(){
 
     return(
         <div className="form-box">
-            <Grid container spacing={2}>
+            <h1>Simule os dados do paciente</h1>
+            <form noValidate>
+                <div className="input-form">
+                    <input 
+                        placeholder="Nome"
+                        type="text"
+                        name="nome"
+                    />
+                </div>
+                <div className="input-form">
+                    <input 
+                        placeholder="Sobrenome"
+                        type="text"
+                        name="sobrenome"
+                    />
+                </div>
+                <div className="input-form">
+                    <input 
+                        placeholder="Diagnóstico"
+                        type="text"
+                        name="diagnostico"
+                    />
+                </div>
+            </form>
+            <section>
+                <article>
+                    <div className="container pb-4">
+                        <div className="row">
+                            <div className="col-lg-3">
+                                <div className="horizontal-card">
+                                    <img src={ImgSPO2}/>
+                                    <div className="horizontal-card-body">
+                                        <span class="card-text">SpO2</span>
+                                        <h4 class="card-title">{values.spo2} %</h4>
+                                    </div>
+                                    <div class="horizontal-card-footer">
+                                        <a class="card-text status">#View</a>
+                                        <a class="card-text status">#Save</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <div className="container pb-4">
+                        <div className="row">
+                            <div className="col-lg-3">
+                                <div className="horizontal-card">
+                                    <img src={ImgFC}/>
+                                    <div className="horizontal-card-body">
+                                        <span class="card-text">Frequência Cardiáca</span>
+                                        <h4 class="card-title">{values.fc} bpm</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            </section>
+            {/* <div className="card-conf">
+                <div className="container pb-4">
+                    <div className="row">
+                        <div className="col-lg-3">
+                            <div className="horizontal-card">
+                                <img src={ImgSPO2}/>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div className="container pb-4">
+                    <div className="row">
+                        <div className="col-lg-3">
+                            <div className="horizontal-card">
+                                <img src={ImgSPO2}/>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div> */}
+            {/* <Grid container spacing={2}>
                 <Grid container justify="center" spacing={3}>
                     <Grid item>
                         <Typography component="h1" variant="h5">Simule os dados</Typography>
@@ -96,10 +181,10 @@ export default function Form(){
                                         <div className="form-card-details">
                                             <CardContent className="form-card-content">
                                                 <Typography componet="h5" variant="h5">
-                                                    SpO2
+                                                    Frequência Cardíaca
                                                 </Typography>
                                                 <Typography variant="subtitle1" color="textSecondary">
-                                                    {values.spo2} %
+                                                    {values.fc} bpm
                                                 </Typography>
                                             </CardContent>
                                             <div className="form-card-controls">
@@ -114,8 +199,8 @@ export default function Form(){
                                         <CardMedia
                                             style={{width: 150}}
                                             component='img'
-                                            image={ImgSPO2}
-                                            title="SPO2"
+                                            image={ImgFC}
+                                            title="FC"
                                         />
                                     </Card>
                                 </Grid>
@@ -141,7 +226,7 @@ export default function Form(){
                         />
                     </Grid>
                 </Grid>
-            </Grid>
+            </Grid> */}
         </div>
     );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './styles.scss';
 import Form from '../../components/Form';
+import Message from '../../components/Message';
 
 export default function Home(){
     return(
-        <div className="box">
+        <div className="container-home">
             <Form />
+            <Message />
         </div>
     );
 }
