8b799dbac5cfa56fd8cadb6b3fd9a6cc84030a53
loressl <loryssl@hotmail.com>
2020-11-05 00:23:22 -0300

    Troca da dependência para criação da mensagem hl7.

--- src/hl7/messageHl7.js
+++ src/hl7/messageHl7.js
@@ -1,93 +1,63 @@
 
-function createMessageHl7(values,signsVitals){
-    const HL7 = require('hl7-standard/index');
-    
-    let hl7= new HL7();
-    const timestamp= new Date();
-    var day= timestamp.getDate();
-    var month= timestamp.getMonth() + 1;
-    var year= timestamp.getFullYear();
-    var strData= year.toString()+month.toString()+day.toString();
+function createMessageHl7(values, signsVitals) {
+    var Writer = require('hl7js').Writer;
 
-    hl7.createSegment('MSH');
-    hl7.set('MSH', {
-        'MSH.2': '^~\\&',
-        'MSH.3': 'Application WEB',
-        'MSH.4': '',
-        'MSH.5': '',
-        'MSH.6': 'Usuário',
-        'MSH.7': strData,
-        'MSH.8': '',
-        'MSH.9': {
-          'MSH.9.1': 'ORM',
-          'MSH.9.2': 'O01'
-        },
-        'MSH.10': '',
-        'MSH.11': '',
-        'MSH.12': '2.5.1',
-        'MSH.13': '',
-        'MSH.14': '',
-        'MSH.15': '',
-        'MSH.16':'AL'
+    const timestamp = new Date();
+    var day = timestamp.getDate();
+    var month = timestamp.getMonth() + 1;
+    var year = timestamp.getFullYear();
+    var strData = year.toString() + month.toString() + day.toString();
+
+    var writer = new Writer({
+        lineSeparator: '0xD 0xA',
+        framePrefix: '',
+        frameSuffix: '',
+        fieldSeparater: '|',
+        componentSeparater: '^',
+        fieldRepeatSeparater: '~',
+        escapeCharacter: '\\',
+        subComponentSeparater: '&'
     });
-    let pidSegment = hl7.createSegmentAfter('PID', hl7.getSegment('MSH'));
-    //hl7.createSegment('PID');
-    pidSegment.set('PID.5', {
-        'PID.5.1': values.lastName,
-        'PID.5.2': values.name
-    });
-    let obrSegment = hl7.createSegmentAfter('OBR', hl7.getSegment('PID'));
-    // hl7.createSegment('OBR');
-    obrSegment.set('OBR',{
-        'OBR.1':'1',
-        'OBR.2':'',
-        'OBR.4':{
-            'OBR.4.2':'Sinais Vitais',
-            'OBR.4.3':'LN'
-        }
-    });
-    let dg1Segment = hl7.createSegmentAfter('DG1', hl7.getSegment('OBR'));
-    // hl7.createSegment('DG1');
-    dg1Segment.set('DG1',{
-        'DG1.4':values.diagnostic
-    });
-    for(var i=0; i < signsVitals.length; i++){
-        if(i === 0){
-            let obxSegment= hl7.createSegmentAfter('OBX', hl7.getSegment('DG1'));
-            //hl7.createSegment('OBX');
-            obxSegment.set('OBX',{
-                'OBX.1':'1',
-                'OBX.2':'NM',
-                'OBX.3':{
-                    'OBX.3.2':signsVitals[i].type.toUpperCase()
-                },
-                'OBX.5':signsVitals[i].value,
-                'OBX.6':{
-                    'OBX.6.1':signsVitals[i].unit
-                },
-                'OBX.7': signsVitals[i].range,
-                'OBX.11':'F'
-            }); 
-        }else{
-            let hl7_obx= hl7.createSegmentAfter('OBX', hl7.getSegment('OBX'));
-            hl7_obx.set('OBX',{
-                'OBX.1':'2',
-                'OBX.2':'NM',
-                'OBX.3':{
-                    'OBX.3.2':signsVitals[i].type.toUpperCase()
-                },
-                'OBX.5':signsVitals[i].value,
-                'OBX.6':{
-                    'OBX.6.1':signsVitals[i].unit
-                },
-                'OBX.7':signsVitals[i].range,
-                'OBX.11':'F'
-            });  
-        }
+
+    writer.addHeader({
+        sendingApplication: 'Application WEB',
+        sendingFacility: '',
+        receivingApplication: '',
+        receivingFacility: 'Usuário',
+        dateTimeOfMessage: strData,
+        security: '',
+        messageType: 'ORM^O01',
+        messageControlId: '',
+        processingId: '',
+        versionId: '2.5.1',
+        sequenceNo: '',
+        continuationPointer: '',
+        acceptAckType: '',
+        applicationAckType: 'AL',
+        countryCode: '',
+        characterSet: '',
+        messageLanguage: ''
+    })
+
+    writer.addSegment('PID', ['', '', '', '', [values.lastName, values.name]])
+    writer.addSegment('OBR', ['1', '', '', ['', 'Sinais Vitais', 'LN']]);
+    writer.addSegment('DG1', ['', '', '', values.diagnostic]);
+    for (var i = 0; i < signsVitals.length; i++) {
+        writer.addSegment('OBX',
+            [
+                i + 1,
+                'NM',
+                [signsVitals[i].type.toUpperCase()],
+                '',
+                signsVitals[i].value,
+                [signsVitals[i].unit],
+                signsVitals[i].range,
+                '', '', '', 'F'
+            ])
     }
-    return hl7.build();
+    return writer.toString();
 }
 
-export default{
+export default {
     createMessageHl7,
 };
